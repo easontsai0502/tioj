8f4de01f1d7db1a5bc24aa15de984e806d03ce30
ET01 <EasonTsai0502@gmail.com>
2022-10-08 14:41:31 +0800

    1001.cpp

---
+++ basic.cpp
@@ -0,0 +1,60 @@
+/*
+[Q]
+[]
+*/
+
+/*include*/
+#include<iostream>
+#include<algorithm>
+#include<cmath>
+#include<string>
+#include<sstream>
+#include<vector>
+#include<queue>
+#include<deque>
+#include<map>
+#include<set>
+#include<stack>
+#include<cstring>
+#include<unordered_map>
+
+/*using namespace*/
+using namespace std;
+
+/*define type*/
+#define ULLI unsigned long long int
+#define LLI long long int
+#define INT int
+#define UINT unsigned INT
+#define PII pair<INT,INT>
+#define PUIUI pair<UINT,UINT>
+
+/*struct*/
+
+/*fn宣告*/
+
+/*num*/
+
+/*fn定義*/
+
+/*main*/
+int main(){
+	{/*IO加速*/
+		cin.tie(0);
+		cout.tie(0);
+		ios::sync_with_stdio(false);
+	}
+	/*CIN*/
+	/*solve*/
+	return 0;
+}
+
+/*
+[I1]
+[O1]
+*/
+
+/*think*/
+/*
+
+*/
