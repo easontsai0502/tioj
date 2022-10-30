020341d49b83245b3852286dfacbc5685c3f73e5
ET01 <EasonTsai0502@gmail.com>
2022-10-30 13:57:49 +0800

    WA

--- 1092.cpp
+++ 1092.cpp
@@ -1,6 +1,8 @@
 /*
 [Q]https://tioj.ck.tp.edu.tw/problems/1092
-[]
+[BFS]
+[Topological sorting]
+[WA]
 */
 
 /*include*/
@@ -50,6 +52,8 @@ INT a,b;
 vector<INT>tree[10000+1];
 vector<INT>backtree[10000+1];
 string fplay;
+INT out[10000+1];
+INT winner[10000+1];
 /*fn定義*/
 /*main*/
 int main(){
@@ -57,26 +61,101 @@ int main(){
 	if(!debug&&iofast)what_the_fuck;
 	while(cin>>n>>e){
 		if(n==0||e==0)break;
-		/*CIN*/
+		/*re:0*/
 		for(INT i=0;i<=n;i++){
 			tree[i].clear();
 			backtree[i].clear();
+			out[i]=0;
+			winner[i]=0;
 		}
+		/*CIN*/
 		for(INT i=0;i<e;i++){
 			cin>>a>>b;
+			a--;
+			b--;
 			tree[a].push_back(b);
 			backtree[b].push_back(a);
+			out[a]++;
 		}
 		cin>>fplay;
-		
 		/*solve*/
+		//bfs
+		//拓撲排序
+		deque<INT>q;
+		q.push_back(n-1);
+		winner[n-1]=0;//數字為0，先的人贏
+		//數字為1，後的人贏
+		while(q.wassomething()){
+			INT now=q.front();
+			q.pop_front();
+			for(INT i:backtree[now]){
+				out[i]--;
+				if(winner[now]==0)winner[i]=1;//贏家swap
+				else winner[i]=0;
+				if(out[i]==0)q.push_back(i);//如果這裡是最後一次通往這，就把他列入代辦清單
+			}
+			if(debug){
+				cout<<now<<":";
+				for(INT i=0;i<n;i++){
+					cout<<winner[i]<<" ";
+				}
+				cout<<endl;
+			}
+		}
+		if(fplay=="Mimi")winner[0]^=1;
+		//假設Moumou先
+		//如果實際是Moumou先開始，其餘正常
+		//如果實際是對方的話，輸贏反向
+		if(winner[0])cout<<"Mimi";
+		else cout<<"Moumou";
+		cout<<endl;
+		if(debug){
+			for(INT i=0;i<n;i++){
+				cout<<winner[i]<<" ";
+			}
+			cout<<endl;
+		}
 	}
 	return 0;
 }
 
 /*
 [I1]
+5 6
+1 2
+1 3
+2 3
+2 4
+3 5
+4 5
+Mimi
+0 0
 [O1]
+Moumou
+[I2]
+5 6
+1 2
+1 3
+2 3
+2 4
+3 5
+4 5
+Moumou
+0 0
+[O2]
+Mimi
+[I3]
+3 2
+1 2
+2 3
+Moumou
+2 1
+1 2
+Moumou
+0 0
+[O3]
+Moumou
+Mimi
 */
 
 /*think*/
